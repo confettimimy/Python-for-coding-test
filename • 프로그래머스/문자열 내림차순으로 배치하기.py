7864d68df929d297685676dbc95e07420fa0dea6
pigmimi <mihyun8889@gmail.com>
2020-11-16 21:37:52 +0900

    Create 문자열 내림차순으로 배치하기.py

---
+++ • 프로그래머스/문자열 내림차순으로 배치하기.py
@@ -0,0 +1,22 @@
+def solution(s):
+    
+    s_list=[] #(문자열) 정렬라이브러리를 사용하기 위해 리스트 자료형을 이용
+    upper = [] #대문자들을 담아둘 임시 리스트
+    
+    for c in s:
+        if c.isupper() == True: #대문자이면
+            upper.append(c)
+        else:
+            s_list.append(c)
+    
+    s_list.sort(reverse=True)
+    upper.sort(reverse=True)
+    
+    s_list.extend(upper)
+    # 현재는 리스트 형태 -> 문자열 형태로 바꾸기
+    result=''
+    result = ''.join(s_list)
+    # print(result) # 확인
+    
+    return result
+
