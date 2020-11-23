f80359a81ae4cc83ffa28d0cff4ebad744ed0c6b
pigmimi <mihyun8889@gmail.com>
2020-11-23 20:32:09 +0900

    Update • 구간 합 계산.py

--- 기타 알고리즘/• 구간 합 계산.py
+++ 기타 알고리즘/• 구간 합 계산.py
@@ -2,5 +2,23 @@
    구간 합 계산을 위해 가장 많이 사용되는 기법이 '접두사 합'이다.
    접두사 합이란 리스트의 맨 앞부터 특정 위치까지의 합을 구해 놓은 것을 의미한다.
 '''
-
 # 구간 합 빠르게 계산하기 알고리즘
+
+
+# 데이터 개수 N과 전체 데이터 선언
+n = 5
+data = [10, 20, 30, 40, 50]
+
+
+# 접두사 합(Prefix Sum) 배열 계산
+sum_value = 0
+prefix_sum = [0]
+for i in data:
+   sum_value += i
+   prefix_sum.append(sum_value)
+
+
+# 구간 합 계산(세 번째 수부터 네 번째 수까지)
+left = 3
+right = 4
+print() ###
