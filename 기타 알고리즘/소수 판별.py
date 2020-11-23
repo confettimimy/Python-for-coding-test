867e2997ed7526374da41d127dbab99306982a52
pigmimi <mihyun8889@gmail.com>
2020-11-23 19:32:07 +0900

    Create 소수 판별.py

---
+++ 기타 알고리즘/소수 판별.py
@@ -0,0 +1,16 @@
+import math
+
+# 소수 판별 함수
+def is_prime_number(x):
+    # 2부터 x의 제곱근까지의 모든 수를 확인하며
+    for i in range(2, int(math.sqrt(x)) + 1):
+        # x가 해당 수로 나누어떨어진다면
+        if x % i == 0:
+            return False # 소수가 아님
+    return True # 소수임
+
+print(is_prime_number(4)) # 4는 소수가 아님
+print(is_prime_number(7)) # 7은 소수임
+
+
+'''특정한 자연수x가 소수인지 확인하기 위하여 바로 가운데 약수까지만 나누어떨어지는지 확인하면 된다.'''
