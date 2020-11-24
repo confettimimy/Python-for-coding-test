9e2217af54120593ee85b609d6113d2ae6539886
pigmimi <mihyun8889@gmail.com>
2020-11-24 20:59:07 +0900

    Update BFS 예제.py

--- DFS BFS/BFS 예제.py
+++ DFS BFS/BFS 예제.py
@@ -9,7 +9,7 @@ def bfs(graph, start, visited):
     # 큐의 구현을 위해 deque 라이브러리 사용
     queue = deque([start])
     # 현재 노드를 방문 처리
-    visited[start] = True
+    visited[start] = True ###[1]
 
     #큐가 빌 때까지 반복
     while queue:
@@ -20,7 +20,7 @@ def bfs(graph, start, visited):
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
-                visited[i] = True
+                visited[i] = True ###[1]
 
 
 # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
@@ -42,3 +42,6 @@ visited = [False] * 9
 
 # 정의된 DFS 함수 호출
 bfs(graph, 1, visited)
+
+
+# [1] append하면 = 즉 값이 큐에 들어오면 방문처리 시키기!
