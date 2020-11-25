ad6d00a6f53dd8299b3ced47cf877b985d7c75b7
pigmimi <mihyun8889@gmail.com>
2020-11-25 16:58:14 +0900

    Create 10815번 숫자 카드.py

---
+++ • 백준 온라인 저지/이분 탐색/10815번 숫자 카드.py
@@ -0,0 +1,24 @@
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start+end) // 2
+        if array[mid] == target:
+            return 1
+        elif array[mid] < target: # 왼쪽 다 잘라버리기
+            start = mid + 1
+        else:
+            end = mid - 1
+    return 0
+
+
+n = int(input())
+a = list(map(int, input().split()))
+a.sort() # 이진탐색 수행 전 정렬돼 있어야함.
+
+m = int(input())
+num_to_find = list(map(int, input().split()))
+
+
+for i in num_to_find:
+    print(binary_search(a, i, 0, len(a)-1 ), end=' ')
+
+
