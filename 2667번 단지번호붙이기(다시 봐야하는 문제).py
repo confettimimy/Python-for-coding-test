5940389e86da0937f747c860ba883ccbeb41fe39
pigmimi <mihyun8889@gmail.com>
2020-11-26 22:12:25 +0900

    Update 2667번 단지번호붙이기(다시 봐야하는 문제).py

--- 2667번 단지번호붙이기(다시 봐야하는 문제).py
+++ 2667번 단지번호붙이기(다시 봐야하는 문제).py
@@ -16,7 +16,7 @@ def dfs(x, y): # (x, y)는 시작 위치 인덱스
     # 집 개수 증가 & 방문체크
     global cnt
     cnt += 1
-    dist[x][y] = True
+    dist[x][y] = True ##### 현재 인덱스 방문처리 해버리고 
 
     # 인접한 노드 탐색하면서 연결되어 있으면 dfs 재귀호출
     for k in range(4): # 네 방향 모두 한 번씩 확인하면서
@@ -25,14 +25,14 @@ def dfs(x, y): # (x, y)는 시작 위치 인덱스
         # 상, 하, 좌, 우 중에서 갈 수 있는 경우
         if 0 <= nx < n and 0 <= ny < n:
             if a[nx][ny] == 1 and dist[nx][ny] == False:
-                dfs(nx, ny)
+                dfs(nx, ny) ##### 연결된 곳도 dfs 재귀 수행해서 또 방문처리 -> 아래 함수 모든 정점 확인 이중 포문에서는 이미 연결된 곳으로 간과됐으니 다른 새로운 시작점을 찾을 수 있는거임.
 
 
 
-# 모든 정점을 확인해서 시작점이 될 수 있는지 확인. 시작점이 가능하면 dfs 탐색 GO.
+# 모든 정점을 확인해서 시작점이 될 수 있는지 확인. 시작점이 가능하면 dfs 탐색 GO
 for i in range(n):
     for j in range(n):
-        if a[i][j] == 1 and dist[i][j] == False:
+        if a[i][j] == 1 and dist[i][j] == False: #####
             print('(',i,',',j,')') # 시작점이 될 수 있는 인덱스 위치 확인용
             
             cnt = 0
