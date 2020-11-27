2fb7f3565f1461e923df0b94b1b6822f64f692ec
pigmimi <mihyun8889@gmail.com>
2020-11-27 15:06:36 +0900

    Create 자릿수 더하기.py

---
+++ • 프로그래머스/자릿수 더하기.py
@@ -0,0 +1,9 @@
+'''자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.'''
+
+def solution(n):
+    
+    ls = list(str(n))
+    ls = map(int, ls)
+    
+    return sum(ls)
