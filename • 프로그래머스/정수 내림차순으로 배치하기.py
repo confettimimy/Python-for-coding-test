468cfe563f1481b57e6674cf420683b18c528629
pigmimi <mihyun8889@gmail.com>
2020-11-27 14:45:38 +0900

    Create 정수 내림차순으로 배치하기.py

---
+++ • 프로그래머스/정수 내림차순으로 배치하기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    n = str(n) #문자열 자료형으로 바꾼 뒤
+    
+    s_list=[]
+    for i in n: #하나씩 꺼내며
+        s_list.append(i) #리스트에 넣는다
+    
+    #이제 리스트 자료형이 되었으니 정렬 라이브러리 사용이 가능하다.
+    s_list.sort(reverse = True) #원본을 바꾼다.
+
+    return int(''.join(s_list))
+
