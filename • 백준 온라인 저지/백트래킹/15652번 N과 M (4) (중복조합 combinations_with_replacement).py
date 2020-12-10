c0cd14e2cdd6ab5d317f1505d8e559f5c8d92346
pigmimi <mihyun8889@gmail.com>
2020-12-10 15:48:33 +0900

    Create

--- • 백준 온라인 저지/백트래킹/15652번 N과 M (4) (중복조합 combinations_with_replacement).py
+++ • 백준 온라인 저지/백트래킹/15652번 N과 M (4) (중복조합 combinations_with_replacement).py
@@ -1,5 +1,5 @@
-# 같은 수를 여러 번 골라도 됨 + 순서 상관 있음 -> 중복 + 순열 
-from itertools import product # 중복순열
+# 같은 수를 여러 번 골라도 됨 + 순서 상관 없음 -> 중복 + 조합
+from itertools import combinations_with_replacement # 중복조합
 
 
 # 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
@@ -9,7 +9,7 @@ ls=[]
 for i in range(1, n+1):
     ls.append(i)
 
-array = sorted(list(product(ls, repeat=m))) # 사전 순으로 증가하는 순서로 출력해야 한다
+array = sorted(list(combinations_with_replacement(ls, m))) # 사전 순으로 증가하는 순서로 출력해야 한다
 
 for case in array:
     
@@ -22,16 +22,10 @@ for case in array:
 1 2 
 1 3 
 1 4 
-2 1 
 2 2 
 2 3 
 2 4 
-3 1 
-3 2 
 3 3 
 3 4 
-4 1 
-4 2 
-4 3 
 4 4 
 '''
