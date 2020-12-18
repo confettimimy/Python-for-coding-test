a96a532b00de6bd59997feb8b9148ad2f25ed76f
pigmimi <mihyun8889@gmail.com>
2020-12-18 17:40:02 +0900

    Update

--- • 프로그래머스/해시/완주하지 못한 선수.py
+++ • 프로그래머스/해시/완주하지 못한 선수.py
@@ -13,3 +13,4 @@ def solution(participant, completion):
     
     return list(answer)[0]
     
+# 동명이인이 존재할 수 있다고 문제에서 명시했기 때문에 특정한 이름을 가진 사람의 수를 담는, 'mislav': 2 이러한 key-value값 형태의 해시 및 딕셔너리 자료형을 이용하는 것이 유도되는 문제이다.
