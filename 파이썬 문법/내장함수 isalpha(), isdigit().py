37cf466829d42141afbaef531ca04b55312eba11
confettimimy <72441809+confettimimy@users.noreply.github.com>
2021-02-02 15:30:06 +0900

    Create 내장함수 isalpha(), isdigit().py

---
+++ 파이썬 문법/내장함수 isalpha(), isdigit().py
@@ -0,0 +1,37 @@
+# 1. '문자열'의 구성이 알파벳인지 확인하기(isalpha)
+
+Ex1 = 'A'
+ 
+Ex2 = 'ABC'
+ 
+Ex3 = "앱피아"
+ 
+Ex4 = "Hello Appia"
+ 
+Ex5 = "100Appia"
+
+print(Ex1.isalpha())
+ 
+print(Ex2.isalpha())
+ 
+print(Ex3.isalpha())
+ 
+print(Ex4.isalpha())
+ 
+print(Ex5.isalpha())
+
+
+
+# 2. '문자열'의 구성이 숫자인지 확인하기(isdigit)
+
+Ex1 = '010-1234-5678'
+ 
+Ex2 = '123456'
+ 
+Ex3 = "R4R3"
+ 
+print(Ex1.isdigit())
+ 
+print(Ex2.isdigit())
+ 
+print(Ex3.isdigit())
