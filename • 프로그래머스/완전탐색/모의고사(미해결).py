a2919aa103204d3a47a5da7b7041869d8e751e04
confettimimy <72441809+confettimimy@users.noreply.github.com>
2021-06-21 22:49:49 +0900

    Update 모의고사(미해결).py

--- • 프로그래머스/완전탐색/모의고사(미해결).py
+++ • 프로그래머스/완전탐색/모의고사(미해결).py
@@ -5,25 +5,22 @@ def solution(answers):
     ls2 = [2, 1, 2, 3, 2, 4, 2, 5]
     ls3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    for a in answers:
-        if a == ls1[a%5]:
+    for i in range(0, len(answers)):
+        if answers[i] == ls1[i%5]:
             counts[0] += 1
-        if a == ls2[a%8]:
+        if answers[i] == ls1[i%8]:
             counts[1] += 1
-        if a == ls3[a%10]:
+        if answers[i] == ls1[i%10]:
             counts[2] += 1
-    print(counts)
+    print(counts,'z') #여기서 일단 무언가가 잘못됨..
     
-    counts.sort(reverse = True)
+    final = [[1], [2], [3]]
+    for k in range(3):
+        final[k].append( counts[k] )
     
+    final.sort(key=lambda x: x[1], reverse = True)
     
-    '''
-    dic = {}
-    dic[1] = counts[0] # 요소 추가
-    dic[2] = counts[1]
-    dic[2] = counts[2]
-    
-    dic.sort(key=lambda x: x[1]) # 맞은 개수 count순으로 정렬
-    
-    return list(dic.keys())'''
-    
+    print(final)
+    print(final[0][0])
+
+    # 딕셔너리 자료형은 리스트와 다르게 sort() 어트리부트를 갖고있지 않는다
