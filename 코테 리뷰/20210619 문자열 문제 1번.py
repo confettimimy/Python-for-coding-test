a74f4098ff57871f7155a405e2c1bb353205a1c5
confettimimy <72441809+confettimimy@users.noreply.github.com>
2021-06-25 03:40:24 +0900

    요기요 1번 다시풀기
    왜 분기문 한쪽에만 드는걸까, 어째서!!

--- 코테 리뷰/20210619 문자열 문제 1번.py
+++ 코테 리뷰/20210619 문자열 문제 1번.py
@@ -27,19 +27,31 @@ def solution(S, C):
         email += "@" + C[0].lower() + C[1:] + ".com>"
 
 
-        # 이제 같은 이메일을 갖고있는 것들을 2, 3과 같은 숫자를 붙여줌으로써 식별해야함
+
+
+        # 이제 같은 이메일을 갖고있는 것들을 2, 3과 같은 숫자를 붙여줌으로써 식별해야함! 여기서 계속 걸렸음
         tmp2 = []
         
         if email in email_ls: # ex: <john.doe@example.com> -> <john.doe2@example.com>
             tmp2 = email.split("@")
-            
-            # '<john.doe @ example.com>
-            if tmp2[0][-1].isdigit() == True: ### 중요
-                if int(tmp2[0][-1]) >= 2:
-                    number = int(tmp2[0][-1])
 
-            number +=1
-            tmp2[0] = tmp2[0] + str(number) + "@"#3...처리도 해야함
+            duplication_estimation = dict() #두 개 이상인 중복메일 넣어둘 곳
+            
+            # '<john.doe @ example.com>'
+            if tmp2[0][-1].isdigit() == True: #숫자가 있고 =이 경우 세번째 등장부터만 가능
+                if int(tmp2[0][-1]) >= 2: #중복됐다면 / 2이면 현재가 3번째 등장이란 뜻 !
+                    duplication_estimation[email] += 1
+                    
+            elif tmp2[0][-1].isalpha() == True and email not in duplication_estimation.keys(): #'<john.doe@example.com>' 이와 같이 숫자가 없는 경우
+                duplication_estimation[email] = 1
+                print(duplication_estimation,'~') #출력시켜보니까 다 여기에 들어가넹..
+                
+            elif tmp2[0][-1].isalpha() == True and email in duplication_estimation.keys(): # 두번째 등장의 경우는 여기에 정리
+                duplication_estimation[email] = 2
+
+
+            print(duplication_estimation,'><')
+            tmp2[0] = tmp2[0] + str(duplication_estimation[email]) + "@"
             email = "".join(tmp2)
 
         email_ls.append(email)
@@ -49,7 +61,7 @@ def solution(S, C):
         # 최종통과되면 최종 답 문자열에 이어붙이기
         S[i] += email_ls[i]
 
-    #print(email_ls)### 확인용
+    print(email_ls)### 확인용
 
 
     
@@ -61,11 +73,6 @@ def solution(S, C):
 
 
     
-    #print(S) #S는 현재 문자열 요소로 구성된 리스트
-    answer = ", ".join(S)
-    
-    return answer
-
 
 
 print(  solution('John Doe, Peter Benjamin Parker, Mary Jane Watson-Parker, John Elvis Doe, John Evan Doe, Jane Doe, Peter Brian Parker', 'Example')  )
