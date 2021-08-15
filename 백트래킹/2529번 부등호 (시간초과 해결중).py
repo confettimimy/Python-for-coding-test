77b291e503846c274adc0aeb426389e9fa742f42
confettimimy <72441809+confettimimy@users.noreply.github.com>
2021-08-16 03:15:50 +0900

    양 옆에서 충족값 찾고 바로 break해도 시간초과 해결 불가능

--- 백트래킹/2529번 부등호 (시간초과 해결중).py
+++ 백트래킹/2529번 부등호 (시간초과 해결중).py
@@ -6,10 +6,11 @@ a = sys.stdin.readline().split()
 
 num_list = [i for i in range(0, 10)]
 
-possible = set()
-first = False
+
 actual_min=0
-for case in list(permutations(num_list, k+1)):
+actual_max=0
+per_list = list(permutations(num_list, k+1))
+for case in per_list: #작은수부터 순서대로 조합 시작
     status = False
     # case = (0, 2, 1)
     s = ''
@@ -17,7 +18,7 @@ for case in list(permutations(num_list, k+1)):
         if len(s)%2 != 0: #홀수이면 2<3과 같이 계산할 수 있는 형태
             if eval(s) == False:
                 status = True # 해당 케이스는 시간낭비 하지 말고 바로 패스
-                continue # 해당 케이스는 시간낭비 하지 말고 바로 패스
+                break # 해당 케이스는 시간낭비 하지 말고 바로 패스
         s += str(case[j])
         s += a[j]
     s += str(case[-1])
@@ -29,26 +30,39 @@ for case in list(permutations(num_list, k+1)):
             for c in s:
                 if c.isdigit():
                     result += c
-            if first==False:
-                actual_min = result #맨처음으로 add되는 값은 어차피 최대값, 시간초과 해결을 위해 최댓값 min(possible)계산을 줄여주자.
-                first=True
-            possible.add(int(result))
+            actual_min = result
+            break # break하면 최종 for루프 바로 나와지는거 확인완료, 여기에 걸려있는 for문이 최종 for루프 밖에 없으니까 
+            #@@@ 맨 처음 충족값 찾자마자 최종 for루프 break. 시간절약을 위해 중간과정은 다 빼고 맨앞부터 제일 첫번째, 맨뒤부터 제일 마지막꺼만 찾으면 됨.
 
+per_list = sorted(per_list, reverse=True)
+for case in per_list: #큰수부터 순서대로 조합 시작
+    status = False
+    
+    s = ''
+    for j in range(k):
+        if len(s)%2 != 0: #홀수이면 2<3과 같이 계산할 수 있는 형태
+            if eval(s) == False:
+                status = True # 해당 케이스는 시간낭비 하지 말고 바로 패스
+                break # 해당 케이스는 시간낭비 하지 말고 바로 패스
+        s += str(case[j])
+        s += a[j]
+    s += str(case[-1])
 
+    if status == False:
 
-#print(possible)
-possible = list(possible)
+        if eval(s) == True:
+            result = ''
+            for c in s:
+                if c.isdigit():
+                    result += c
+            actual_max = result
+            break
+            #@@@ 맨 처음 충족값 찾자마자 최종 for루프 break.
+        
 
-print(max(possible))
+print(actual_max)
+print(actual_min)
 
-# 21(X), 021(O) : 최솟값 앞이 0이면 int시 지워지므로 0이 달린 원본값으로 다시 만들어줘야함
-min_result = '' 
-short = len(str(actual_min))
-if short != (k+1): 
-    min_result = '0'*((k+1)-short)  +  str(actual_min)
-    print(min_result)
-else:
-    print(actual_min)
 
 
 '''시간초과 해결하기
