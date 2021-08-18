3a8bd48a23087d2c90106044da98f391c77d1ab7
confettimimy <72441809+confettimimy@users.noreply.github.com>
2021-08-18 20:44:17 +0900

    Update continue.py

--- 파이썬 문법/continue.py
+++ 파이썬 문법/continue.py
@@ -9,3 +9,9 @@ for i in range(10):
 
 # if문에서 break를 써버리면 for문 전체를 빠져나와버림
 # continue를 쓰면 해당 아래코드를 건너뛰고 다시 루프문에 들게 할 수 있다.
+
+
+'''continue는 반복문에서 사용된다.
+   [case1] for문에서 continue로 코드 실행 건너뛰기
+   [case2] while 반복문에서 continue로 코드 실행 건너뛰기
+# 
